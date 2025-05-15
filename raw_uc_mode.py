062030a17f0dedd56b62ca6d1c52f1e4f3700d1a
pledgebox <max@pledgebox.com>
2025-05-15 11:19:48 +0800

    only one sb

--- raw_uc_mode.py
+++ raw_uc_mode.py
@@ -3,20 +3,8 @@ from seleniumbase import SB
 import os
 import time
 
-
-with SB(uc=True, test=True) as sb:
-    url = "https://seleniumbase.io/hobbit/login"
-    sb.uc_open_with_disconnect(url, 4)
-    sb.uc_gui_press_keys("\t ")
-    sb.reconnect(3)
-    print(sb.get_current_url())
-    sb.assert_text("Welcome to Middle Earth!", "h1")
-    sb.set_messenger_theme(location="bottom_center")
-    sb.post_message("SeleniumBase wasn't detected!")
-
 with SB(uc=True, test=True) as sb:
     url = os.getenv("url")
     sb.open(url)
     time.sleep(5)
     print(sb.get_page_title())
-    
